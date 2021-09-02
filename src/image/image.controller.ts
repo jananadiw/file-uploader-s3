6ec8ee75d2d63df6874639df14a85d025fe4c04d
Jananadi Wedagedara <jananadi92@gmail.com>
2021-09-02 23:13:12 +0900

    fix: upload files to s3 bucket

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,35 +1,17 @@
-import {
-  Controller,
-  Post,
-  UploadedFiles,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, Req, Res } from '@nestjs/common';
 import { ImageService } from './image.service';
-import * as multerS3 from 'multer-s3';
-import * as AWS from 'aws-sdk';
-import 'dotenv/config';
 
-const s3 = new AWS.S3();
-
-@Controller('image')
+@Controller('fileupload')
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
-
   @Post()
-  @UseInterceptors(
-    FilesInterceptor('images', 3, {
-      storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET_NAME,
-        acl: 'public-read',
-        key: (req, file, cb) => {
-          cb(null, file.originalname);
-        },
-      }),
-    }),
-  )
-  async uploadImage(@UploadedFiles() files: Express.Multer.File) {
-    return this.imageService.uploadImage(files);
+  async create(@Req() request, @Res() response) {
+    try {
+      await this.imageService.fileupload(request, response);
+    } catch (error) {
+      return response
+        .status(500)
+        .json(`Failed to upload image file: ${error.message}`);
+    }
   }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Req, Res, Injectable } from '@nestjs/common';
+import * as multer from 'multer';
 import * as AWS from 'aws-sdk';
+import * as multerS3 from 'multer-s3';
+import 'dotenv/config';
 
-AWS.config.update({
-  accessKeyId: 'process.env.AWS_ACCESS_KEY_ID',
-  secretAccessKey: 'process.env.AWS_SECRET_ACCESS_KEY',
-  region: 'process.env.AWS_REGION',
-});
-
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
 const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 @Injectable()
 export class ImageService {
-  async uploadImage(files) {
-    return 'SUCCESS';
+  async fileupload(@Req() req, @Res() res) {
+    try {
+      this.upload(req, res, function (error) {
+        if (error) {
+          console.log(error);
+          return res.status(404).json(`Failed to upload image file: ${error}`);
+        }
+        return res.status(201).json(req.files[0].location);
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(`Failed to upload image file: ${error}`);
+    }
   }
+
+  upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: AWS_S3_BUCKET_NAME,
+      acl: 'private',
+      key: function (request, file, cb) {
+        cb(null, `${Date.now().toString()} - ${file.originalname}`);
+      },
+    }),
+  }).array('upload', 1);
 }
